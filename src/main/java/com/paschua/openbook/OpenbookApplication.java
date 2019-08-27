a615b7c7a6a687b5ba71aa6ba980b4c17d25220c
Fernando Paschualetto <paschualetto@gmail.com>
2019-08-26 21:27:12 -0300

    Creating project using Spring Initializr

---
+++ src/main/java/com/paschua/openbook/OpenbookApplication.java
@@ -0,0 +1,13 @@
+package com.paschua.openbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paschua/openbook/OpenbookApplicationTests.java
@@ -0,0 +1,16 @@
+package com.paschua.openbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpenbookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

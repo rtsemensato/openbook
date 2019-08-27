1858d2f40f662d86dbeb697766c2a5294643cc21
Fernando Celestino Paschualetto <fernando.paschualetto@matera.com>
2019-08-27 10:33:42 -0300

    Welcome Screen

---
+++ src/test/java/com/paschua/openbook/controller/WelcomeControllerTest.java
@@ -0,0 +1,36 @@
+package com.paschua.openbook.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.Matchers.containsString;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = WelcomeController.class)
+public class WelcomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void main() throws Exception {
+        ResultActions resultActions = mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("welcome"))
+                .andExpect(model().attribute("message", equalTo("Dear Reader")))
+                .andExpect(content().string(containsString("Hello, Dear Reader")));
+
+        MvcResult mvcResult = resultActions.andReturn();
+        ModelAndView mv = mvcResult.getModelAndView();
+    }
+}
